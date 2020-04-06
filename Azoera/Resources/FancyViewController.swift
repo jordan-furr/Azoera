f2e0657d4e6168176520ce2daff965410bea575c
Jordan Furr <you@example.com>
2020-04-06 14:23:01 -0400

    some

---
+++ Azoera/Resources/FancyViewController.swift
@@ -0,0 +1,46 @@
+//
+//  FancyViewController.swift
+//  Azoera
+//
+//  Created by Jordan Furr on 4/6/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class FancyViewController: UIViewController {
+
+    
+    @IBOutlet weak var loginbutton: UIButton!
+    @IBOutlet weak var signupbutton: UIButton!
+    @IBOutlet weak var signmeupbutton: UIButton!
+    @IBOutlet weak var emailtextfield: UITextField!
+    @IBOutlet weak var faqbutton: UIButton!
+    
+    @IBOutlet weak var confirmpasswordtextfield: UITextField!
+    @IBOutlet weak var passwordtextfield: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
